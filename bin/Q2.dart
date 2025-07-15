3e0d1a042dc227006ce91f2c43a8955cf908d34e
SHadya_Fathy <shadyafathy80@gmail.com>
2025-07-16 01:36:48 +0300

    Add q2 and q3 solutions

---
+++ bin/Q2.dart
@@ -0,0 +1,12 @@
+import'dart:io';
+
+void main(){
+ print("please,choose the number:");
+  int number =int .parse(stdin.readLineSync()!);
+  if(number%2==0){
+        print("choosen number is even");
+  }
+  else{
+     print("choosen number is even");
+  }
+}
